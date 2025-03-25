2325e5f14da87a7c69074b66310ce4dcbc31f795
Luke Campbell <luke@axiomdatascience.com>
2025-03-25 14:32:39 -0400

    Formatting

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
-use urlencoding::encode;
 use clap::Parser;
-
+use urlencoding::encode;
 
 #[derive(Parser)]
 struct Args {
     /// The thing to encode using RFC 3986 URL Path and Query encoding
     target: Vec<String>,
 }
-
 
 /// Parse and print the command arguments as a RFC-3986 compatible URL Encoded
 /// String.
